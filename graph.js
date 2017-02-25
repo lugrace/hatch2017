bb95c19546c16b33f55d73aee39d6be8abfbadd7
Grace Lu <2018glu@tjhsst.edu>
2017-02-25 15:39:22 -0500

    weather stuff

--- graph.js
+++ graph.js
@@ -1,33 +1,37 @@
 
 var id = 5815135;
-var start = 1369728000;
-var cnt = 5; //One per hour
+var start = 1451606400; //Jan 1, 2016
+var end = 1459468800; //April 1, 2016
+var day = 86400 //One day in unix time
 // var url = 'http://history.openweathermap.org/data/2.5/history/city?id=' + id +"&type=hour&start="+start+"&cnt="cnt+"&APPID="+mykey;
-var url = "http://api.openweathermap.org/data/2.5/weather?q=Washington&mode=json&appid="+mykey;
+
 // var jsonW = JSON.parse(getJSON(url));
-temp = 'bar';
+var temps = [];
+var labels = [];
 
-$.getJSON(url, function(data) {
-    //data is the JSON string
-    console.log('hooray');
-    console.log(data.weather);
-});
-
+for(var i = start; i < end; i+=day){
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=Washington&mode=json&units=imperial&start="+i+"&appid="+mykey;
+  //var url = "http://api.openweathermap.org/data/2.5/history/weather?q=Washington&mode=json&units=imperial&start="+i+"&appid="+mykey;
+  $.getJSON(url, function(data) {
+      //data is the JSON string
+      // console.log('hooray');
+    temps.push(data.main.temp);
+    labels.push(i);
+  });
+}
+console.log(temps);
+console.log(labels);
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
 //type: 'bar',
-type: temp,
+type: 'line',
 data: {
-  labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+  labels: labels, //['M', 'T', 'W', 'T', 'F', 'S', 'S']
   datasets: [{
-    label: 'apples',
-    data: [12, 19, 3, 17, 6, 3, 7],
+    label: 'temperatures',
+    data: temps, //[12, 19, 3, 17, 6, 3, 7]
     backgroundColor: "rgba(153,255,51,0.4)"
-  }, {
-    label: 'oranges',
-    data: [2, 29, 5, 5, 2, 3, 10],
-    backgroundColor: "rgba(255,153,0,0.4)"
-}]},
+  }]},
 options: {
   title :{
     display:true,

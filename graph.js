8b972488e9df9c13d4a657f04dfb2c04e0f02bd1
Grace Lu <2018glu@tjhsst.edu>
2017-02-26 01:19:30 -0500

    allowing ranges now-- time to sleep :^)

--- graph.js
+++ graph.js
@@ -1,8 +1,5 @@
 
 var id = 5815135;
-// var start = 1451606400; //Jan 1, 2016
-// var end = 1459468800; //April 1, 2016
-var day = 86400 //One day in unix time
 // var url = 'http://history.openweathermap.org/data/2.5/history/city?id=' + id +"&type=hour&start="+start+"&cnt="cnt+"&APPID="+mykey;
 
 // var jsonW = JSON.parse(getJSON(url));
@@ -10,6 +7,8 @@ var temps = [];
 var labels = [];
 var arr = [];
 var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+// var ori = 'lmao';
+// var end = 'lol';
 
   // var url = "https://api.openweathermap.org/data/2.5/weather?q=Washington&mode=json&units=imperial&start="+i+"&appid="+mykey;
   //var url = "http://api.openweathermap.org/data/2.5/history/weather?q=Washington&mode=json&units=imperial&start="+i+"&appid="+mykey;
@@ -23,24 +22,53 @@ $.getJSON(url, function(data) {
       var s = (myDate.toLocaleString());
       labels.push(s);
       temps.push(arr[i].main.temp);
-    };
+    }
+    makeChart();
   });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-//type: 'bar',
-type: 'line',
-data: {
-  labels: labels,
-  datasets: [{
-    label: 'temperatures',
-    data: temps,
-    backgroundColor: "rgba(153,255,51,0.4)"
-  }]},
-options: {
-  title :{
-    display:true,
-    text: "Washington DC's (5815135) Weather"
+function updateTemps(){
+  var x = document.getElementById('frm1');
+  var fdate = str(x.elements[0].value);
+  var ldate = str(x.elements[1].value);
+  if(fdate != "" && ldate != ""){
+    labels = []
+    temps = []
+    var myfDate = new Date(fdate); // Your timezone!
+    var mylDate = new Date(ldate);
+    var myfUnix = myfDate.getTime()/1000.0;
+    var mylUnix = mylDate.getTime()/1000.0;
+    for(var j = 0; j < arr.length; j++){
+      uni = arr[j].dt;
+      if(uni >= myfUnix && uni <= mylUnix){
+        var myDate = new Date(uni*1000);
+        var s = (myDate.toLocaleString());
+        labels.push(s);
+        temps.push(arr[j].main.temp);
+      }
+    }
+    //February 27, 2017 4:00:00
+    //March 1, 2017 4:00:00
   }
+  makeChart();
 }
-});
+
+function makeChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+  //type: 'bar',
+  type: 'line',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'temperatures',
+      data: temps,
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }]},
+  options: {
+    title :{
+      display:true,
+      text: "Washington DC's (5815135) Weather"
+    }
+  }
+  });
+}
